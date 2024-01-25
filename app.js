fa7dae25d4572aa4e896b5384e7ad7a3b6b1770f
Darwin Manzano <manza_92@yahoo.com>
2024-01-24 21:10:00 -0600

    ejercicio en js

--- app.js
+++ app.js
@@ -1,21 +1,108 @@
-let titulo = document.querySelector("h1");
-titulo.innerHTML = "Hora del Desafío";
+// // let titulo = document.querySelector("h1");
+// // titulo.innerHTML = "Hora del Desafío";
 
-function botonClicado() {
-  alert("El boton fue clicado");
+// // function botonClicado() {
+// //   alert("El boton fue clicado");
+// // }
+
+// // function mostrarAlerta() {
+// //   let ciudad = prompt("Por favor ingresa el nombre de una ciudad de Brasil:");
+// //   alert("Estuve en " + ciudad + " y me acorde de ti");
+// // }
+// // function mostrarMensaje() {
+// //   alert("Amo JS");
+// // }
+
+// // function sumaDosNumeros() {
+// //   let primerNumero = parseInt(prompt("Dijite el primer número"));
+// //   let segundoNumero = parseInt(prompt("Dijite el sengundo número"));
+// //   let resultado = primerNumero + segundoNumero;
+// //   alert(`${primerNumero} + ${segundoNumero} = ${resultado}`);
+// // }
+
+
+// // function verificarNumero(numero) {
+// //   if (numero > 0) {
+// //     console.log("El número es positivo.");
+// //   } else if (numero < 0) {
+// //     console.log("El número es negativo.");
+// //   } else {
+// //     console.log("El número es zero.");
+// //   }
+// // }
+
+// //Crear una función que muestra "¡Hola, mundo!" en la consola
+
+// function mostrarHola() {
+//   console.log("¡Hola, mundo!");
+// }
+
+// mostrarHola();
+
+// //Crear una función que recibe un nombre como parámetro y muestra "¡Hola, [nombre]!" en la consola.
+
+// function mostrarHolaNombre(nombre) {
+//   console.log(`¡Hola, ${nombre}!`);
+// }
+
+// mostrarHolaNombre("Darwin");
+
+// //Crear una función que recibe un número como parámetro y devuelve el doble de ese número.
+
+// function calcularDoble(numero) {
+//   return numero * 5;
+// }
+
+// let resultadoDoble = calcularDoble(5);
+// console.log(resultadoDoble);
+
+// //Crear una función que recibe tres números como parámetros y devuelve su promedio.
+
+// function calcularPromedio(a,b,c) {
+//     return (a + b + c) / 3;
+// }
+// let promedio = calcularPromedio(4, 7 , 10);
+// console.log(promedio)
+
+// //Crear una función que recibe dos números como parámetros y devuelve el mayor de ellos.
+
+// function encontrarMayor(a,b){
+//     return a > b ? a : b;
+// }
+// let numeroMayor = encontrarMayor(15, 9);
+// console.log(numeroMayor)
+
+
+// //Crear una función que recibe un número como parámetro y devuelve el resultado de multiplicar ese número por sí mismo.
+
+// function cuadrado(numero) {
+//     return numero * numero;
+
+// }
+
+// let numero = cuadrado(2)
+// console.log(resultado)
+
+let nota1 = 7;
+let nota2 = 6;
+let nota3 = 3;
+let nota4 = 5;
+
+
+function calcularPromedio(nota1, nota2, nota3, nota4){
+    return;
 }
 
-function mostrarAlerta() {
-  let ciudad = prompt("Por favor ingresa el nombre de una ciudad de Brasil:");
-  alert("Estuve en " + ciudad + " y me acorde de ti");
-}
-function mostrarMensaje() {
-  alert("Amo JS");
+function verificarAprobacion(promedio){
+    return;
+
 }
 
-function sumaDosNumeros() {
-  let primerNumero = parseInt(prompt("Dijite el primer número"));
-  let segundoNumero = parseInt(prompt("Dijite el sengundo número"));
-  let resultado = primerNumero + segundoNumero;
-  alert(`${primerNumero} + ${segundoNumero} = ${resultado}`);
+function calcularPromedio(nota1, nota2, nota3, nota4){
+    let promedio = (nota1 + nota2 + nota3 + nota4)
+    return promedio ;
+}
+
+function verificarAprobacion(promedio){
+         return promedio >= 5 ? 'Aprobado' : 'Reprobado';
 }
